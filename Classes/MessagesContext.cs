7dd546058e0dabcca6fc41a9fb0168692a7dc5b6
student-a502 <student-a502@STUD-RM21-A502>
2025-11-10 16:04:25 +0500

    Реализованные классы реализующие интерфейсы. Коневский

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pr13.Models;
+using Pr13.Interfaces;
+
+namespace Pr13.Classes
+{
+    public class MessagesContext: Messages, IMessages
+    {
+        public static List<Messages> AllMessages;
+        public MessagesContext() => All(out AllMessages);
+        public MessagesContext(string Message, DateTime Create, int IdUser) : base(Message, Create, IdUser) { }
+        public void All(out List<Messages> Messages) => Messages = new List<Messages>();
+        public void Delete() => AllMessages.Remove(this);
+        public void Save(bool Update = false)
+        {
+            if(!Update)
+                AllMessages.Add(this);
+        }
+    }
+}
---
+++ Classes/UserContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pr13.Interfaces;
+using Pr13.Models;
+
+namespace Pr13.Classes
+{
+    public class UserContext : Users, IUsers
+    {
+        public List<Users> AllUsers;
+        public UserContext() => this.All(out AllUsers);
+        public void All(out List<Users> Users)
+        {
+            Users = new List<Users>();
+            AllUsers.Add(new Users(1, "Аликина Ольга"));
+            AllUsers.Add(new Users(2, "Бояркин Данил"));
+            AllUsers.Add(new Users(3, "Бурмантов Владислав"));
+            AllUsers.Add(new Users(4, "Дылдин Максим"));
+            AllUsers.Add(new Users(5, "Евдокимов Даниил"));
+            AllUsers.Add(new Users(6, "Костюнин Никита"));
+            AllUsers.Add(new Users(7, "Кучин Данил"));
+            AllUsers.Add(new Users(9, "Мотырев Александр"));
+            AllUsers.Add(new Users(10, "Мухридинов Далер"));
+            AllUsers.Add(new Users(11, "Олейник Владимир"));
+            AllUsers.Add(new Users(12, "Саблин Константин"));
+            AllUsers.Add(new Users(13, "Субботин Валерий"));
+            AllUsers.Add(new Users(14, "Сукрушев Егор"));
+            AllUsers.Add(new Users(15, "Торсунов Даниил"));
+            AllUsers.Add(new Users(16, "Хабибрахманов Никита"));
+            AllUsers.Add(new Users(17, "Хикматулин Григорий"));
+            AllUsers.Add(new Users(18, "Черенев Сергей"));
+            AllUsers.Add(new Users(19, "Чупин Дмитрий"));
+            AllUsers.Add(new Users(20, "Шилов Дмитрий"));
+        }
+    }
+}
