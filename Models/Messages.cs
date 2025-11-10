8f6c3469cebd201616cbc16894dc7309b3ebfd1a
student-a502 <student-a502@STUD-RM21-A502>
2025-11-10 15:23:11 +0500

    Реализованные модели. Коневский

---
+++ Models/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr13.Models
+{
+    internal class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create {  get; set; }
+        public int IdUser { get; set; }
+        public Messages() { }
+        public Messages(string Message, DateTime Create, int IdUser) { }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr13.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Users() { }
+        public Users(int Id, string FIO) { }
+    }
+}
